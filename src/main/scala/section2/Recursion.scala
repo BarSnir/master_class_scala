91fba314d24aaa739b8690ab38b0255c9d4c70ed
Bar Snir <bars@Admins-MacBook-Pro.local>
2022-04-20 23:33:04 -0400

    added more learning

--- src/main/scala/section2/Recursion.scala
+++ src/main/scala/section2/Recursion.scala
@@ -24,4 +24,37 @@ object Recursion extends App {
     /*
         When u need loops, Iterate with Tail recursion.
     */
+
+    def concatenateTailrec(myString: String, n: Int, accumulator: String): String = {
+        if (n <= 0) accumulator
+        else concatenateTailrec(myString, n-1, accumulator+myString)
+    }
+
+    println(concatenateTailrec("Hello Bar", 5000, ""))
+
+
+    def isPrime(myInt: Int): Boolean = {
+        def isPrimeTailrec(t: Int, isStillPrime: Boolean): Boolean = {
+            if (!isStillPrime) false
+            else if (t <= 1) true
+            else isPrimeTailrec(t-1, myInt % t != 0 && isStillPrime)
+        }
+        isPrimeTailrec(myInt/2, true)
+    }
+
+    println(isPrime(2003))
+    println(isPrime(679))
+
+
+    def fibonacciFunc(n: Int): BigInt = {
+        def fibonacciFuncTailrec(i: Int, last: Int, prev: Int): BigInt = {
+            if (i >= n) last
+            else fibonacciFuncTailrec(i+1, last+prev, last)
+        }
+        if (n <= 2) 1
+        else fibonacciFuncTailrec(2, 1, 1)
+    }
+
+    println(fibonacciFunc(2000))
+
 }
